750f0edfc1f2b390ea49ca3e503ff2d93c8b08f7
Akriti <akriti.extratech@outlook.com>
2024-02-07 14:35:26 +0545

    First commit -Feb7

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./style.css";
+
+function App() {
+  return (
+    <>
+      <div className="heading">
+        <b>AKRITI GHIMIRE</b>
+      </div>
+      <div className="img">
+        <img className="image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUDpWEGrFDm9aPU8r-R3UeE0FFBtf3p69t_w&usqp=CAU" alt="profile"/>
+      </div>
+      <div className="description">
+        <b>Full Stack Developer</b>
+      </div>
+      <br />
+      <div className="description">
+        <i>
+          Mail to:
+          <a href="akriti7ghimire@gmail.com">akriti7ghimire@gmail.com</a>{" "}
+        </i>
+      </div>
+    </>
+  );
+}
+
+export default App;
